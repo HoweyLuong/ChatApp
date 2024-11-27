f81fc2376e158edda996908a5e32e4d6f5fa3313
HoweyLuong <165950202+HoweyLuong@users.noreply.github.com>
2024-11-27 11:26:41 -0800

    Added the new user in the chat

--- app/src/main/java/com/example/chatapp/activities/userActivity.java
+++ app/src/main/java/com/example/chatapp/activities/userActivity.java
@@ -2,17 +2,19 @@ package com.example.chatapp.activities;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.chatapp.R;
+import com.example.chatapp.adapters.UsersAdapter;
 import com.example.chatapp.databinding.ActivityUserBinding;
+import com.example.chatapp.models.User;
+import com.example.chatapp.utilities.Constants;
 import com.example.chatapp.utilities.PreferenceManager;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class userActivity extends AppCompatActivity {
 
@@ -22,9 +24,10 @@ public class userActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityUserBinding.inflate(getLayoutInflater());
+        preferenceManager = new PreferenceManager(getApplicationContext());
         setContentView(binding.getRoot());
         setListeners();
-        //getUser();
+        getUser();
     }
 
     private void setListeners() {
@@ -32,19 +35,56 @@ public class userActivity extends AppCompatActivity {
     }
 
     private void getUser() {
+    loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        database.collection(Constants.KEY_COLLECTION_USERS).get()
+                .addOnCompleteListener(task -> {
+                    loading(false);
+                    String currentUserId= preferenceManager.getString(Constants.KEY_USER_ID);
+                    if(task.isSuccessful() && task.getResult()!= null) {
+                        List<User> users = new ArrayList<>();
+                        for(QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()) {
+                            if(currentUserId.equals(queryDocumentSnapshot.getId())) {
+                                continue;
+                            }
+                           User user = new User();
+                            user.name = queryDocumentSnapshot.getString(Constants.KEY_FIRST_NAME);
+                            user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
+                            user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
+                            user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                            user.id = queryDocumentSnapshot.getId();
+                            users.add(user);
 
+
+
+
+                        }
+
+                        if(users.size() > 0) {
+                            UsersAdapter usersAdapter = new UsersAdapter(users);
+                            binding.userRecyclerView.setAdapter(usersAdapter);
+                            binding.userRecyclerView.setVisibility(View.VISIBLE);
+
+                        }else {
+                            showErrorMesssage();
+                        }
+                    }else {
+                        showErrorMesssage();
+                    }
+                });
     }
 
 
-    private void showToast(String message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    private void showErrorMesssage() {
+        binding.textErrorMessage.setText(String.format("%s","No user available"));
+        binding.textErrorMessage.setVisibility(View.VISIBLE);
 
     }
     private void loading(Boolean isLoading) {
         if(isLoading) {
-            binding.progressBar.setVisibility(View.VISIBLE);
-        }else {
             binding.progressBar.setVisibility(View.INVISIBLE);
+        }else {
+            binding.progressBar.setVisibility(View.VISIBLE);
         }
     }
 }
